0f21dab562910a7b63c4ce306c8ffb6a7d375d80
G00423199@atu.ie <g00423199@atu.ie>
2023-10-17 14:18:10 +0100

    added strings

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,11 @@
 package ie.atu;
 
 public class Person {
+    String firstName = "";
+    String lastName = "";
+    int age = 0;
+
+    public void displayInfo() {
+        System.out.println("Name: " + firstName + " " + lastName + ", Age: " + age);
+    }
 }
