bc8b61aebe3d98f763f91109d5f080b8d6baed0b
G00423199@atu.ie <g00423199@atu.ie>
2023-10-17 14:25:02 +0100

    added constructors

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,10 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //creates a new person object
+        Person johnDoe = new Person("Joshua", "Battle", 20  );
+
+        //calling the displayinfo method
+        johnDoe.displayInfo();
     }
 }
