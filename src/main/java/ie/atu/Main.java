c996dc9e50831edc6b71c372cb6880398cb76a9d
G00423199@atu.ie <g00423199@atu.ie>
2023-10-17 14:36:55 +0100

    added user input feature to person class and main

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,7 +3,10 @@ package ie.atu;
 public class Main {
     public static void main(String[] args) {
         //creates a new person object
-        Person johnDoe = new Person("Joshua", "Battle", 20  );
+        Person user = new Person();
+
+        //get user input
+        user.getUserInput();
 
         //calling the displayinfo method
         johnDoe.displayInfo();
